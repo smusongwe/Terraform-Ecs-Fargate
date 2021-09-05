5f8f711c9e391f4a0426346e83d2608f5d47cc65
Bkoji1150 <kojibello058@gmail.com>
2021-09-05 17:45:53 -0400

    uppdate on ecs and variable

--- variable.tf
+++ variable.tf
@@ -26,7 +26,7 @@ variable "app_port" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "735972722491.dkr.ecr.us-west-2.amazonaws.com/haplet-registory:latest"
+  default     = "nginx:latest"
 }
 
 variable "fargate_cpu" {
