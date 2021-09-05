f41b7c875eb6f458f1c9cfb34eaf3423b5d60057
Bkoji1150 <85324828+Bkoji1150@users.noreply.github.com>
2021-09-05 15:21:25 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -24,7 +24,7 @@ resource "aws_alb_target_group" "app" {
   }
 }
 
-# Redirect all traffic from the ALB to the target group
+# #Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.main.id
   port              = var.app_port
