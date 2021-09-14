cf31347f99c5bd95bc9582be3c87737e480782fe
smusongwe <musongwe94@gmail.com>
2021-09-13 21:44:45 -0400

    update bacnkend file

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "tf-project-omega"
-    key     = "path/projectomega/haplet.tfstate"
-    region  = "us-west-2"
-    profile = "default"
-    dynamodb_table = "tf-project-omega"
+    bucket         = "tf-project-omega2"
+    key            = "path/projectomega/haplet.tfstate"
+    region         = "us-west-2"
+    profile        = "default"
+    dynamodb_table = "tf-project-omega2"
   }
 }
